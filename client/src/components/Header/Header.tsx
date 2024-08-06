5a9f3b8f67ddc4e32c9cc64473b2ceecbe3e98a8
daud <alpn.ds@mail.ru>
2024-08-06 16:30:15 +0300

    header_refactored_to_fix_burger

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -3,13 +3,17 @@ import HeaderTop from "./HeaderTop";
 import HeaderMiddle from "./HeaderMiddle";
 import HeaderBottom from "./HeaderBottom";
 import "./header.scss";
+import BurgerMenu from "./BurgerMenu";
+import {useToggler} from "../../hooks/useToggler";
 
 export const Header: FC = () => {
+    const [burgerVisible, burgerClickHandler] = useToggler(false);
     return (
         <>
+            <BurgerMenu burgerVisible={burgerVisible} burgerClickHandler={burgerClickHandler}/>
             <header className="header">
                 <div className="header__wrapper">
-                    <HeaderTop/>
+                    <HeaderTop burgerClickHandler={burgerClickHandler}/>
                 </div>
             </header>
             <HeaderMiddle/>
--- client/src/components/Header/HeaderTop.tsx
+++ client/src/components/Header/HeaderTop.tsx
@@ -4,24 +4,28 @@ import HeaderUserActions from "./HeaderUserActions";
 import Burger from "./Burger";
 import SearchButton from "../Search/SearchButton";
 import SearchPopup from "../Search/SearchPopup";
-import BurgerMenu from "./BurgerMenu";
 import {useToggler} from "../../hooks/useToggler";
 
-const HeaderTop: FC = () => {
+interface IHeaderTop {
+    burgerClickHandler: () => void,
+}
+
+const HeaderTop: FC<IHeaderTop> = ({burgerClickHandler}) => {
     const [searchVisible, searchClickHandler] = useToggler(false);
-    const [burgerVisible, burgerClickHandler] = useToggler(false);
+
 
     return (
-        <div className="header__top">
-            <div className="header__top-wrapper container">
-                <HeaderNavigation/>
-                <HeaderUserActions/>
-                <SearchButton clickHandler={searchClickHandler}/>
-                <SearchPopup visible={searchVisible} setSearchVisibleHandler={searchClickHandler}/>
-                <Burger clickHandler={burgerClickHandler}/>
-                <BurgerMenu burgerVisible={burgerVisible} burgerClickHandler={burgerClickHandler}/>
+        <>
+            <div className="header__top">
+                <div className="header__top-wrapper container">
+                    <HeaderNavigation/>
+                    <HeaderUserActions/>
+                    <SearchButton clickHandler={searchClickHandler}/>
+                    <SearchPopup visible={searchVisible} setSearchVisibleHandler={searchClickHandler}/>
+                    <Burger clickHandler={burgerClickHandler}/>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
