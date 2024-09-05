737b61a9296d02bccf867610d0777317e5ee862d
daud <alpn.ds@mail.ru>
2024-09-05 23:31:17 +0300

    search_fxd

--- client/src/components/Search/Search.tsx
+++ client/src/components/Search/Search.tsx
@@ -1,6 +1,6 @@
 import React, {FC, useState} from "react";
 import "./search.scss";
-import {NavLink} from "react-router-dom";
+import {useNavigate} from "react-router";
 
 interface ISearch {
     setVisibleHandler: (arg: boolean) => void,
@@ -8,22 +8,31 @@ interface ISearch {
 }
 
 const Search: FC<ISearch> = ({setVisibleHandler}) => {
+    const navigate = useNavigate();
     const [search, setSearch] = useState('');
 
+    function handler() {
+        if (search.replace(/\s+/g, '')) {
+            setVisibleHandler(false);
+            navigate('/search/' + search);
+
+        }
+
+    }
 
     return (
         <section className="search__wrapper">
             <div className="search__form">
-                <input className="search__input" type="search" value={search}
+                <input onKeyUp={({key}) => {
+                    if (key === 'Enter') handler();
+                }} className="search__input" type="search" value={search}
                        onChange={(e) => setSearch(e.target.value)} maxLength={100}
                        placeholder="введите поисковый запрос"/>
-                <NavLink onClick={() => setVisibleHandler(false)} to={'/search/' + search}>
-                    <svg className="search__submit" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                         stroke="currentColor" stroke-width="2">
-                        <circle cx="11" cy="11" r="8"/>
-                        <path d="M21 21l-4.35-4.35"/>
-                    </svg>
-                </NavLink>
+                <svg onClick={handler} className="search__submit" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                     stroke="currentColor" stroke-width="2">
+                    <circle cx="11" cy="11" r="8"/>
+                    <path d="M21 21l-4.35-4.35"/>
+                </svg>
             </div>
         </section>
     );
